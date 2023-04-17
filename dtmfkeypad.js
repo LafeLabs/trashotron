9fa11a48ca97c4a93b8e9f93c077a2bac32a81c6
Trash Robot <lafelabs@gmail.com>
2023-04-16 22:46:35 -0600

    finally got the dtmf keypad to work with nice looking keys

--- dtmfkeypad.js
+++ dtmfkeypad.js
@@ -11,82 +11,26 @@ let osc, fft;
 frameIndex = -30;
 
 function setup() {
-  createCanvas(720, 256);
+  createCanvas(innerWidth, innerHeight);
 
   osc = new p5.SinOsc(); // set frequency and type
   osc.amp(0);
   osc2 = new p5.SinOsc(); // set frequency and type
   osc2.amp(0);
   fft = new p5.FFT();
-  osc.start();
-  osc2.start();
 
   freq = 697;
   osc.freq(freq);  
 
-  press1 = createButton('1');
-  press1.position(0, 0);
-  press1.mousePressed(one)
-  press2 = createButton('2');
-  press2.position(20, 0);
-  press2.mousePressed(two)
-  press3 = createButton('3');
-  press3.position(40, 0);
-  press3.mousePressed(three)
-  press4 = createButton('4');
-  press4.position(60, 0);
-  press4.mousePressed(four)
-
-  press5 = createButton('5');
-  press5.position(80, 0);
-  press5.mousePressed(five)
-  press6 = createButton('6');
-  press6.position(100, 0);
-  press6.mousePressed(six)
-  press7 = createButton('7');
-  press7.position(120, 0);
-  press7.mousePressed(seven)
-  press8 = createButton('8');
-  press8.position(140, 0);
-  press8.mousePressed(eight)
-
-  
-  press9 = createButton('9');
-  press9.position(160, 0);
-  press9.mousePressed(nine)
-  press0 = createButton('0');
-  press0.position(180, 0);
-  press0.mousePressed(zero)
-  pressstar = createButton('*');
-  pressstar.position(200, 0);
-  pressstar.mousePressed(star)
-  presspound = createButton('#');
-  presspound.position(220, 0);
-  presspound.mousePressed(pound)
-
-  
-  pressA = createButton('A');
-  pressA.position(240, 0);
-  pressA.mousePressed(A)
-  pressB = createButton('B');
-  pressB.position(260, 0);
-  pressB.mousePressed(B)
-  pressC = createButton('C');
-  pressC.position(280, 0);
-  pressC.mousePressed(C)
-  pressD = createButton('D');
-  pressD.position(300, 0);
-  pressD.mousePressed(D)
-
-  
 }
 
 
 function draw() {
 
-  background(255);
+  background(159,135,103);
 
   let waveform = fft.waveform(); // analyze the waveform
+  noFill();
   beginShape();
   strokeWeight(5);
   for (let i = 0; i < waveform.length; i++) {
@@ -95,6 +39,7 @@ function draw() {
     vertex(x, y);
   }
   endShape();
+      
 
   // change oscillator frequency based on mouseX
 
@@ -366,3 +311,14 @@ function D(){
 }
 
 
+function mousePressed(){
+  osc.start();
+  osc2.start();
+  return false();
+}
+
+function keyPressed() {
+  osc.start();
+  osc2.start();
+  return false; // prevent any default behaviour
+}
